279baa84915d80d86a62ec3b88e28259a6ba993a
Caeson Reckling <167999918+Creck20@users.noreply.github.com>
2025-08-18 18:03:49 +0000

    Type casting

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,29 @@
 #include <cstdint>
 
 int main(){
-    
+    float target_x;
+    int32_t sprite_x;
+    uint32_t player_x;
+
+    // Not putting f at the end of the expression turns it into a double hover mouse to see.
+    target_x = -123.45;
+
+    // Implicit type conversion: expect -123
+    sprite_x = target_x;
+
+    // player_x does not support negative numbers, so the data will be stored in memory as the two's compliment number.
+    player_x = sprite_x;
+
+    // Print out types with implicit casting:
+    std::cout << "target_x (float) " << target_x << std::endl;
+    std::cout << "sprite_x (int32) " << sprite_x << std::endl;
+    std::cout << "player_x (unsigned int32) " << player_x << "\n\n";
+
+    // Print out types with explicit casting: 
+    std::cout << "player_x (unsigned int32) " << static_cast<int32_t>(player_x) << std::endl;
+
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -9,7 +9,14 @@ int main(){
     int fahrenheit = 100;
     int celsius;
 
-    celsius = fahrenheit;
+    // Performing interger division: Quotient of 5/9 = 0
+    // celsius = (5 / 9) * (fahrenheit - 32);
+
+    // Two valid options used: explicitly cast 5 to float and convert 9 to 9.0 (a double)
+    // celsius = (static_cast<float>(5) / 9.0) * (fahrenheit - 32);
+
+    // Also seems to work if you add the f to make 5.0 and 9.0 floats.
+    celsius = (5.0f / 9.0f) * (fahrenheit - 32);
 
     std::cout << std::endl;
     std::cout << "Fahrenheit: " << fahrenheit << std::endl;
@@ -19,8 +26,10 @@ int main(){
     
     std::cout << std::endl;
     std::cout << "Float          : " << weight << std::endl;
-    std::cout << "Integer part   : " << weight << std::endl;
-    std::cout << "Fractional part: " << weight << std::endl;
+    // Static cast weight to int
+    std::cout << "Integer part   : " << static_cast<int>(weight) << std::endl;
+    // C style cast used to get the above value, subtract decimal part and move decimla point 4 spaces to the left. Note that the data is not 9900. 
+    std::cout << "Fractional part: " << (int)((weight - (int)weight) * 10000) << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
