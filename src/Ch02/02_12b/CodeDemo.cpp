dee32f37eddbd727a3a60994dc6676e6fcde8a58
Caeson Reckling <167999918+Creck20@users.noreply.github.com>
2025-08-18 18:48:39 +0000

    auto typecasting

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -6,13 +6,22 @@
 #include <typeinfo>
 
 int main(){
-    auto score;
-    auto points;
-    auto height;
-    auto duration;
-    auto is_active;
-    auto initial;
-    auto title;
+
+    // Explicit declaration required for auto.
+    // Int
+    auto score = 8;
+    // Long int
+    auto points = 12345678901;
+    // Float
+    auto height = 6.2f;
+    // Double
+    auto duration = 9.0;
+    // Bool
+    auto is_active = true;
+    //Char ''
+    auto initial = 'P';
+    // String ""
+    auto title = "Soccer Champions";
 
     std::cout << "The type of score is " << typeid(score).name() << std::endl;
     std::cout << "The type of points is " << typeid(points).name() << std::endl;
@@ -20,6 +29,7 @@ int main(){
     std::cout << "The type of duration is " << typeid(duration).name() << std::endl;
     std::cout << "The type of is_active is " << typeid(is_active).name() << std::endl;
     std::cout << "The type of initial is " << typeid(initial).name() << std::endl;
+    // Pkc stands for pointer to const-char or string
     std::cout << "The type of title is " << typeid(title).name() << std::endl;
 
     std::cout << std::endl << std::endl;
