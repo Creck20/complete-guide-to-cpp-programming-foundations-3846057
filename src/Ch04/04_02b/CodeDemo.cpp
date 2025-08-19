baf66249a7d2db71e7898d0ca6d6a9b0628a431f
Caeson Reckling <167999918+Creck20@users.noreply.github.com>
2025-08-19 21:38:16 +0000

    Pointer explanation

--- src/Ch04/04_02b/CodeDemo.cpp
+++ src/Ch04/04_02b/CodeDemo.cpp
@@ -7,15 +7,20 @@
 
 int main(){
     int a = 37;
+    // A pointer variable is declared using an * in the identifier
     int *ptr;
 
-    ptr = nullptr;
+    // The address of A: & is the "address of" operator
+    ptr = &a;
 
-    std::cout << "             The value of a is " << ptr << std::endl;
+    std::cout << "             The value of a is " << a << std::endl;
     std::cout << "    ptr is pointing to address " << ptr << std::endl;
-    std::cout << "           The address of a is " << ptr << std::endl;
-    std::cout << "Where ptr is pointing, we have " << ptr << std::endl;
-    std::cout << "         The address of ptr is " << ptr << std::endl;
+    // This should be the same as above
+    std::cout << "           The address of a is " << &a << std::endl;
+    // This should be the value of a
+    std::cout << "Where ptr is pointing, we have " << *ptr << std::endl;
+    // This is were the address of a is stored 
+    std::cout << "         The address of ptr is " << &ptr << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
